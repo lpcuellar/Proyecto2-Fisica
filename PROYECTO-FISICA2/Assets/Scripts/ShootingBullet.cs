a3474678188849873eec082a4dd6e1bebc114567
Luis Pedro Cuéllar <lpcuellar14@gmail.com>
2019-05-21 13:35:06 -0600

    You can shoot now

---
+++ PROYECTO-FISICA2/Assets/Scripts/ShootingBullet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingBullet : MonoBehaviour
+{
+    public float speed = 0f;
+    public float angle = 0f;
+    public float mass;
+    public Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D >();
+        rb.mass = mass;
+
+        rb.velocity = transform.right * speed;
+    }
+
+
+
+}
