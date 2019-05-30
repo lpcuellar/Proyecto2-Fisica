bbe71ff7d4906575c4e203b0ca99e22600f03ff9
DELLE6420\CjrBarrios <cjrbarrios@gmail.com>
2019-05-30 16:37:45 -0600

    final

--- PROYECTO-FISICA2/Assets/Scripts/Plataforma.cs
+++ PROYECTO-FISICA2/Assets/Scripts/Plataforma.cs
@@ -1,14 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Plataforma : MonoBehaviour
 {
+    public Text AltuTxt;
     public GameObject plat;
+    void Start()
+    {
+        AltuTxt = GameObject.Find("Altura").GetComponent<Text>();
+    }
     public void Slider_Changed(float newValue)
     {
         Vector3 pos = plat.transform.position;
         pos.y = newValue;
         plat.transform.position = pos;
+        var altnormal = newValue + 10.63;
+        var cero = 0;
+        if (altnormal>=0) {
+            AltuTxt.text = altnormal.ToString();
+        }
+        else
+        {
+            AltuTxt.text = cero.ToString();
+        }
     }
 }
